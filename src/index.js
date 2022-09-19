e7c57a46b34aafecc3664260b208e51d889fd9de
ninichuuh <105721940+ninichuuh@users.noreply.github.com>
2022-09-19 23:08:12 +0200

    final vise u pm

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { BrowserRouter as Router } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-import { BrowserRouter as Router } from "react-router-dom";
+if(process.env.NODE_ENV==='production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
